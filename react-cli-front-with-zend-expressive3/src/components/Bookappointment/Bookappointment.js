e7b8e6003b1eeffd3578e543091208b7a804538a
pradeep <pradeep.srivatav@rsystems.com>
2018-10-09 13:03:39 +0530

    react cli test

--- react-cli-front-with-zend-expressive3/src/components/Bookappointment/Bookappointment.js
+++ react-cli-front-with-zend-expressive3/src/components/Bookappointment/Bookappointment.js
@@ -1,11 +1,49 @@
 // @flow
 import React, { Component} from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
+import ReactDOM from 'react-dom';
+import { Link  } from 'react-router-dom';
 
 class Bookappointment extends Component {
 	static defaultProps: Object;
 	
-	state = {};
+	state = {
+		records: [],		
+		full_name:'',
+		appointment_time:'',
+		appointment_reason:'',
+		popupAdd:'',
+		createID:'',
+	};
+	
+	componentWillUnmount(){
+		this.setState({ full_name : '' });
+		this.setState({ appointment_time : '' });
+		this.setState({ appointment_reason : '' });
+	}
+	
+	bookAppointment(){
+		if(this.state.full_name.value){
+		let body = new FormData();			
+				body.append('full_name', this.state.full_name.value);
+				body.append('appointment_time', this.state.appointment_time.value);
+				body.append('appointment_reason', this.state.appointment_reason.value);
+				
+		axios.post(`http://127.0.0.1:8080/createappointment`, body)
+		  .then(res => {
+				console.log(res); 
+					if(res.data.status){
+						this.setState({				
+							popupAdd:!res.data.status,
+							createID:0
+						});						
+						this.props.history.push(`/`)
+					}			
+							
+		  })
+		}
+	} 
  
 	render() {
 		return (
@@ -21,27 +59,27 @@ class Bookappointment extends Component {
 
 <div className="row">
 <div className="label">Full Name</div>
-<div className="input_field"><input type='text'    id='full_name' name='full_name' />
+<div className="input_field"><input type='text'  ref={(input) => this.state.full_name = input}  id='full_name' name='full_name'   />
 
 </div>
 </div> 
 
 <div className='row'>
 <div className='label'>Select Date and Time</div>
-<div className='input_field'><input type='text'   name="appointment_time" id='appointment_time' value="2018-09-26 03:53" /></div>
+<div className='input_field'><input type='text'   name="appointment_time" id='appointment_time' ref={(input) => this.state.appointment_time = input}  /></div>
 
 </div> 
 
 <div className='row'>
 <div className='label'>Reason of Appointment</div>
-<div className='input_field'><textarea rows='9' cols='50'    id='appointment_reason' name='appointment_reason' ></textarea></div>
+<div className='input_field'><textarea  ref={(input) => this.state.appointment_reason = input} rows='9' cols='50' id='appointment_reason' name='appointment_reason'  /></div>
 
 </div> 
 
 <div className='row'>
 <div className='label'>&nbsp;</div>
 <div className='input_field form_update'>
-	 <button type="submit">Submit</button>
+	 <button type="button" onClick={()=>this.bookAppointment()}>Submit</button>
 </div>
 </div> 
 
